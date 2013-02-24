d55c792aa04f02f126f6e1426a170bb1eafca2ab
lincolnnus <shaohuan.li@gmail.com>
2013-02-25 05:55:32 +0800

    Added Booking View

--- HotelPromotion/Managers/HPCredentialsManager.h
+++ HotelPromotion/Managers/HPCredentialsManager.h
@@ -11,10 +11,13 @@
 @interface HPCredentialsManager : NSObject
 @property (nonatomic, strong) HPUser *currentUser;
 @property (nonatomic, strong) NSArray *promotions;
+@property (nonatomic, strong) NSArray *bookings;
+
 + (HPCredentialsManager *)sharedInstance;
 - (void)loginAsCustomer;
 - (void)loginAsStaff;
 - (void)updatePromotions;
+- (void)updateBookings;
 - (void)insertNewPromotion:(NSDictionary *)dictionary;
 - (void)insertNewBooking: (NSDictionary *)dictionary;
 @end
--- HotelPromotion/ViewControllers/HPBookingsTableViewController.m
+++ HotelPromotion/ViewControllers/HPBookingsTableViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "HPBookingsTableViewController.h"
+#import "HPBookingTableViewCell.h"
+#import "HPBookingDetailViewController.h"
 
-@interface HPBookingsTableViewController ()
+@interface HPBookingsTableViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSArray *bookings;
 @end
 
 @implementation HPBookingsTableViewController
@@ -27,7 +31,75 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     [self.tabBarItem setTitle:@"My Bookings"];
+    
+    [self setupTableView];
+    [[HPCredentialsManager sharedInstance] updateBookings];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(refreshTableView) name:@"updateBookingsSucceed" object:nil];
 }
+
+
+- (void)setupTableView {
+    CGRect tableViewFrame = CGRectMake(0, 0, self.view.size.width, self.view.size.height);
+    UITableView *tv = [[UITableView alloc] initWithFrame:tableViewFrame style:UITableViewStylePlain];
+    self.tableView = tv;
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.view addSubview:self.tableView];
+}
+
+- (void)refreshTableView {
+    self.bookings = [[HPCredentialsManager sharedInstance] bookings];
+    [self.tableView reloadData];
+}
+
+#pragma mark - UITableView datasource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.bookings.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *contactTableViewCellIdentifier = @"BookingViewCell";
+    HPBookingTableViewCell *cell = (HPBookingTableViewCell *)[tableView dequeueReusableCellWithIdentifier:contactTableViewCellIdentifier];
+    if (cell == nil) {
+        NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"HPBookingTableViewCell" owner:self options:nil];
+        cell = [nib objectAtIndex:0];
+    }
+    
+    HPBooking *bookings = [self.bookings objectAtIndex:indexPath.row];
+    // cell.textLabel.text= (NSString*)bookings.startDate;
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"dd/MM/YYYY"];
+    cell.textLabel.text = [formatter stringFromDate:bookings.bookTime];
+    cell.detailTextLabel.text = bookings.confirmationNum;
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    //    HPPromotion *promotion = [self.promotions objectAtIndex:indexPath.row];
+    //    HPPromotionDetailViewController *detailVC = [[HPPromotionDetailViewController alloc] initWithPromotion:promotion];
+    //    [self.navigationController pushViewController:detailVC animated:YES];
+    
+    [self performSegueWithIdentifier:@"BookingDetailSegue" sender:self];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"BookingDetailSegue"]) {
+        /*
+         HPPromotionDetailViewController *detailVC = (HPPromotionDetailViewController *)segue.destinationViewController;
+         NSIndexPath *indexPath = (NSIndexPath *)sender;
+         HPPromotion *promotion = [self.promotions objectAtIndex:indexPath.row];
+         detailVC.promotion = promotion;
+         */
+        
+        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+        HPBooking *bookings= [self.bookings objectAtIndex:indexPath.row];
+        HPBookingDetailViewController *detailVC = (HPBookingDetailViewController *)segue.destinationViewController;
+        detailVC.booking = bookings;
+    }
+}
+
 
 - (void)didReceiveMemoryWarning
 {
